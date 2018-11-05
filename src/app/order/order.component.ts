fab2e88aae542f14d574524ad99a312223a67763
Le Xuan Quynh <quynhlx@Thanhs-iMac.local>
2018-11-05 21:02:32 +0700

    order screen

--- src/app/order/order.component.ts
+++ src/app/order/order.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ITable } from '../interfaces/ITable';
 import { TableService } from '../services/table.service';
+import { IFoodFilter } from '../interfaces/IFoodFilter';
 
 @Component({
   selector: 'app-order',
@@ -10,12 +11,51 @@ import { TableService } from '../services/table.service';
 })
 export class OrderComponent implements OnInit {
   table: ITable;
+  filters: IFoodFilter[];
   constructor(private route: ActivatedRoute, private tableService: TableService) { }
 
   ngOnInit() {
     const id = parseInt(this.route.snapshot.params.tableId);
     this.tableService.getTables();
     this.table = this.tableService.getTableById(id);
+
+    this.filters = [{
+      icon: 'assets/coffee',
+      title: 'Coffee',
+      active: true
+    }, {
+      icon: 'assets/tea-bag',
+      title: 'Tea'
+    },
+    {
+      icon: 'assets/coffee',
+      title: 'Coffee',
+    }, {
+      icon: 'assets/tea-bag',
+      title: 'Tea'
+    },
+    {
+      icon: 'assets/coffee',
+      title: 'Coffee',
+    }, {
+      icon: 'assets/tea-bag',
+      title: 'Tea'
+    },
+    {
+      icon: 'assets/coffee',
+      title: 'Coffee',
+    }, {
+      icon: 'assets/tea-bag',
+      title: 'Tea'
+    }]
+  }
+
+  selectFilter(filter: IFoodFilter) {
+    this.filters = this.filters.map(f => {
+      f.active = false;
+      return f;
+    });
+    filter.active = true;
   }
 
 }
